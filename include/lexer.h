a811dd91b3859c5667ad0c177d22832a7fabffd0
Daniel Juarez <djuarez@car12s6.42barcelona.com>
2025-09-15 01:12:54 +0200

    updated 09/15 01/12

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: djuarez <djuarez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:28:28 by djuarez           #+#    #+#             */
-/*   Updated: 2025/09/15 01:06:58 by djuarez          ###   ########.fr       */
+/*   Updated: 2025/09/15 01:11:50 by djuarez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,31 @@
 # include "libft.h"
 # include <stdbool.h>
 
-//Define token types
+/* ---------------------------- Token types ---------------------------- */
 typedef enum e_token_type
 {
-	TOKEN_WORD,// normal word   #0
-	TOKEN_PIPE,// pipe '|'      #1
-	TOKEN_REDIRECT_IN,// <		#2
-	TOKEN_REDIRECT_OUT,// >		#3
-	TOKEN_HEREDOC,// <<			#4
-	TOKEN_APPEND,// >>			#5
-	TOKEN_EOF,// end of file 	#6
-	TOKEN_IN_FILE,// Input file #7
-	TOKEN_OUT_FILE// Output file#8
+	TOKEN_WORD,         // normal word   #0
+	TOKEN_PIPE,         // pipe '|'      #1
+	TOKEN_REDIRECT_IN,  // <             #2
+	TOKEN_REDIRECT_OUT, // >             #3
+	TOKEN_HEREDOC,      // <<            #4
+	TOKEN_APPEND,       // >>            #5
+	TOKEN_EOF,          // end of file   #6
+	TOKEN_IN_FILE,      // Input file    #7
+	TOKEN_OUT_FILE      // Output file   #8
 }	t_token_type;
 
-//Quotes
+/* ---------------------------- Quote types ---------------------------- */
 typedef enum e_quote_type
 {
-	QUOTE_NONE,   //#0
-	QUOTE_SINGLE, //#1
-	QUOTE_DOUBLE, //#2
-	QUOTE_MIXED,  //#3
-	QUOTE_DOLLAR  //#4
+	QUOTE_NONE,    // #0
+	QUOTE_SINGLE,  // #1
+	QUOTE_DOUBLE,  // #2
+	QUOTE_MIXED,   // #3
+	QUOTE_DOLLAR   // #4
 }	t_quote_type;
 
-//Fragment structure
+/* ---------------------------- Structures ----------------------------- */
 typedef struct s_fragment 
 {
 	char				*text;
@@ -51,11 +51,10 @@ typedef struct s_fragment
 	bool				has_space_after;
 	bool				is_variable;
 	struct s_fragment	*next;
-} t_fragment;
+}	t_fragment;
 
-//Token structure
-//Function prototype moved below the fragment structure
-char *concat_fragments(t_fragment *fragments);
+char	*concat_fragments(t_fragment *fragments);
+
 typedef struct s_token 
 {
 	t_token_type	type;
@@ -63,44 +62,39 @@ typedef struct s_token
 	bool			has_space_before;
 	char			*final_text;
 	struct s_token	*next;
-} t_token;
+}	t_token;
 
-// --- MISSING FUNCTION PROTOTYPES FOR TOKE
-t_token *create_token(t_token_type type, bool has_space_before);
-t_fragment *new_fragment(const char *text, size_t len, t_quote_type quote_type, bool has_space_after);
-void append_fragment(t_fragment **list, t_fragment *new_frag);
-void free_fragments(t_fragment *frag);
-void free_token_list(t_token *tokens);
+/* ----------------------- Structure management ------------------------ */
+t_token		*create_token(t_token_type type, bool has_space_before);
+t_fragment	*new_fragment(const char *text, size_t len,
+				t_quote_type quote_type, bool has_space_after);
+void		append_fragment(t_fragment **list, t_fragment *new_frag);
+void		free_fragments(t_fragment *frag);
+void		free_token_list(t_token *tokens);
 
-enum lexer_state 
-{
-	LS_NORMAL,
-	LS_IN_SINGLE,
-	LS_IN_DOUBLE,
-	LS_ESCAPE
-};
+/* ------------------------ Tokenization core -------------------------- */
+t_token		*tokenize_input(const char *input);
+int			is_quote(char c);
+int			is_operator(char c);
+t_token_type	determine_token_type(char *str, t_quote_type quote);
+t_token		*append_token_eof(t_token *head);
 
-//(Tokenize/lexer)utils
-t_token			*tokenize_input(const char *input);
-int				is_quote(char c);
-int				is_operator(char c);
-t_token_type 	determine_token_type(char *str, t_quote_type quote);
-t_token			*append_token_eof(t_token *head);
-char			*remove_quotes(char *str);
-bool			are_quotes_closed(const char *input);
-char			*extract_quoted_segment(const char *input, int *len);
-int				skip_spaces(const char *s, int i);
-char			*handle_quoted_part(const char *input, int *i, char *tmp,
-					t_quote_type *last_quote);
-char			*handle_plain_text(const char *input, int *i, char *tmp);
-int 			check_unmatched_quotes(const char *input);
-t_fragment 		*parse_mixed_fragments(const char *text);
-t_fragment		*handle_backslashes(const char *text, int *i);
+/* -------------------------- Quote helpers ---------------------------- */
+char		*remove_quotes(char *str);
+bool		are_quotes_closed(const char *input);
+char		*extract_quoted_segment(const char *input, int *len);
+int			check_unmatched_quotes(const char *input);
+t_fragment	*parse_mixed_fragments(const char *text);
+t_fragment	*handle_backslashes(const char *text, int *i);
 
-// Debug functions
-void			print_fragments(t_fragment *fragments);
-void 			print_tokens_raw(t_token *tokens);
+/* ------------------------- Parsing helpers --------------------------- */
+int			skip_spaces(const char *s, int i);
+char		*handle_quoted_part(const char *input, int *i, char *tmp,
+				t_quote_type *last_quote);
+char		*handle_plain_text(const char *input, int *i, char *tmp);
 
+/* ----------------------------- Debug -------------------------------- */
+void		print_fragments(t_fragment *fragments);
+void		print_tokens_raw(t_token *tokens);
 
 #endif
-
