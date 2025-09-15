b825dfa8c9395ee6626f7420439ed3fc119663a0
Daniel Juarez <djuarez@car10s6.42barcelona.com>
2025-09-15 19:12:46 +0200

    updated 09/15 18/33

--- src/lexer/lexer_utils1.c
+++ src/lexer/lexer_utils1.c
@@ -6,7 +6,7 @@
 /*   By: djuarez <djuarez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 14:09:48 by djuarez           #+#    #+#             */
-/*   Updated: 2025/09/15 01:31:16 by djuarez          ###   ########.fr       */
+/*   Updated: 2025/09/15 19:12:05 by djuarez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,25 @@ char	*remove_quotes(char *str)
 
 	if (!str)
 		return (NULL);
-
 	len = ft_strlen(str);
-
-	// Elimina solo comillas dobles externas
 	if (len > 1 && str[0] == '"' && str[len - 1] == '"')
 		result = ft_substr(str, 1, len - 2);
 	else
 		result = ft_strdup(str);
-
 	return (result);
 }
 
-t_token *create_token(t_token_type type, bool space_before) 
+t_token	*create_token(t_token_type type, bool space_before)
 {
-    t_token *tok = malloc(sizeof(t_token));
-    if (!tok)
-        return NULL;
+	t_token	*tok;
 
-    tok->type = type;
-    tok->fragments = NULL;
-    tok->has_space_before = space_before;
-    tok->final_text = NULL;
-    tok->next = NULL;
-    return tok;
+	tok = malloc(sizeof(t_token));
+	if (!tok)
+		return (NULL);
+	tok->type = type;
+	tok->fragments = NULL;
+	tok->has_space_before = space_before;
+	tok->final_text = NULL;
+	tok->next = NULL;
+	return (tok);
 }
-
-
