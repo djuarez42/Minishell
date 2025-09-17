3fcac837ff2b8afc037124602908b35d092188b3
Daniel Juarez <djuarez@car7s6.42barcelona.com>
2025-09-17 18:37:19 +0200

    update 17/09

--- include/exec_state.h
+++ include/exec_state.h
@@ -6,7 +6,7 @@
 /*   By: djuarez <djuarez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 20:26:39 by djuarez           #+#    #+#             */
-/*   Updated: 2025/09/15 18:42:37 by djuarez          ###   ########.fr       */
+/*   Updated: 2025/09/17 18:13:26 by djuarez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 typedef struct s_exec_state
 {
-    int last_status;
-}   t_exec_state;
+	int	last_status;
+}	t_exec_state;
 
 #endif
