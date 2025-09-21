752517e0a788f9805534044d3c914b04db808d00
Daniel Juarez <djuarez@car3s6.42barcelona.com>
2025-09-21 19:21:26 +0200

    update 21/09 19:15

--- src/executor/executor_utils4.c
+++ src/executor/executor_utils4.c
@@ -6,7 +6,7 @@
 /*   By: djuarez <djuarez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 18:52:48 by djuarez           #+#    #+#             */
-/*   Updated: 2025/09/21 19:14:34 by djuarez          ###   ########.fr       */
+/*   Updated: 2025/09/21 19:19:48 by djuarez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,6 @@ int	execute_execve(char *exec_path, char **argv, char **envp)
 	return (0);
 }
 
-
 void	free_split(char **split)
 {
 	int	i;
@@ -97,7 +96,7 @@ void	free_split(char **split)
 	free(split);
 }
 
-int handle_redirections_and_quotes(t_redir *redirs, char **envp, t_exec_state *state)
+/*int handle_redirections_and_quotes(t_redir *redirs, char **envp, t_exec_state *state)
 {
 	t_redir		*redir;
 	t_fragment	*frag;
@@ -136,6 +135,63 @@ int handle_redirections_and_quotes(t_redir *redirs, char **envp, t_exec_state *s
 			free(redir->file);
 			redir->file = tmp;
 		}
+		redir = redir->next;
+	}
+	res = handle_redirections(redirs, envp, state);
+	return (res);
+}*/
+
+static int process_heredoc(t_redir *redir)
+{
+	t_fragment	*frag;
+	bool		is_quoted;
+	char		*built_delim;
+
+	frag = redir->fragments;
+	is_quoted = false;
+	while (frag)
+	{
+		if (frag->quote_type == QUOTE_SINGLE || frag->quote_type == QUOTE_DOUBLE)
+		{
+			is_quoted = true;
+			break ;
+		}
+		frag = frag->next;
+	}
+	redir->quoted = is_quoted;
+	built_delim = build_heredoc_delimiter(redir->file);
+	if (!built_delim)
+		return (1);
+	free(redir->file);
+	redir->file = built_delim;
+	return (0);
+}
+
+static void process_remove_quotes(t_redir *redir)
+{
+	char *tmp;
+
+	tmp = remove_quotes(redir->file);
+	free(redir->file);
+	redir->file = tmp;
+}
+
+int handle_redirections_and_quotes(t_redir *redirs, char **envp, t_exec_state *state)
+{
+	t_redir	*redir;
+	int		res;
+
+	redir = redirs;
+	while (redir)
+	{
+		if (redir->type == TOKEN_HEREDOC)
+		{
+			res = process_heredoc(redir);
+			if (res != 0)
+				return (1);
+		}
+		else
+			process_remove_quotes(redir);
 		redir = redir->next;
 	}
 	res = handle_redirections(redirs, envp, state);
