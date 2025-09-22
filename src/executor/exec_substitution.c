fb95ce4abe7f6fe9cfe8bb79c4440ee91f77f547
ekakhmad <ekakhmad@student.42.fr>
2025-09-22 19:18:27 +0200

    src/executor/exec_substitution.c

--- src/executor/exec_substitution.c
+++ src/executor/exec_substitution.c
@@ -6,33 +6,30 @@
 /*   By: ekakhmad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 22:41:32 by ekakhmad          #+#    #+#             */
-/*   Updated: 2025/09/21 22:41:43 by ekakhmad         ###   ########.fr       */
+/*   Updated: 2025/09/22 19:11:14 by ekakhmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*execute_command_substitution(const char *cmd, char **envp)
+static pid_t	spawn_command_subst(const char *cmd, char **envp, int *read_fd)
 {
 	int		fd[2];
 	pid_t	pid;
-	char	buffer[1024];
-	int		n;
-	char	*result;
-	size_t	size;
-	char	*argv[] = {"/bin/sh", "-c", (char *)cmd, NULL};
-	char	*tmp;
+	char	*argv[4];
 
-	result = NULL;
-	size = 0;
+	argv[0] = "/bin/sh";
+	argv[1] = "-c";
+	argv[2] = (char *)cmd;
+	argv[3] = NULL;
 	if (pipe(fd) == -1)
-		return (NULL);
+		return (-1);
 	pid = fork();
 	if (pid < 0)
 	{
 		close(fd[0]);
 		close(fd[1]);
-		return (NULL);
+		return (-1);
 	}
 	else if (pid == 0)
 	{
@@ -43,27 +40,50 @@ char	*execute_command_substitution(const char *cmd, char **envp)
 		execve("/bin/sh", argv, envp);
 		_exit(127);
 	}
-	else
+	close(fd[1]);
+	*read_fd = fd[0];
+	return (pid);
+}
+
+static char	*read_command_output_and_wait(int read_fd, pid_t pid)
+{
+	char	buffer[1024];
+	ssize_t	n;
+	char	*result;
+	size_t	size;
+	char	*tmp;
+
+	size = 0;
+	result = NULL;
+	n = read(read_fd, buffer, sizeof(buffer));
+	while (n > 0)
 	{
-		close(fd[1]);
-		while ((n = read(fd[0], buffer, sizeof(buffer))) > 0)
+		tmp = realloc(result, size + n + 1);
+		if (!tmp)
 		{
-			tmp = realloc(result, size + n + 1);
-			if (!tmp)
-			{
-				free(result);
-				result = NULL;
-				break ;
-			}
-			result = tmp;
-			memcpy(result + size, buffer, n);
-			size += n;
+			free(result);
+			result = NULL;
+			break ;
 		}
-		if (result)
-			result[size] = '\0';
-		close(fd[0]);
-		waitpid(pid, NULL, 0);
+		result = tmp;
+		memcpy(result + size, buffer, n);
+		size += n;
+		n = read(read_fd, buffer, sizeof(buffer));
 	}
+	if (result)
+		result[size] = '\0';
+	close(read_fd);
+	waitpid(pid, NULL, 0);
 	return (result);
 }
 
+char	*execute_command_subst(const char *cmd, char **envp)
+{
+	int		read_fd;
+	pid_t	pid;
+
+	pid = spawn_command_subst(cmd, envp, &read_fd);
+	if (pid == -1)
+		return (NULL);
+	return (read_command_output_and_wait(read_fd, pid));
+}
